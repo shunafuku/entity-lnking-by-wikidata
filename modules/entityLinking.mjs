03506603d6e82b694bcf73f6d411f643a572b7e5
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-05-21 01:41:29 +0900

    Update entityLinking.mjs

--- modules/entityLinking.mjs
+++ modules/entityLinking.mjs
@@ -4,67 +4,70 @@ import entityDecide from "./entityDecide.mjs";
 import fetchCategory from "./category.mjs";
 import createLinkedText from "./createLinkedText.mjs";
 
-function isNgWord(word, ngWords) {
-  if (ngWords.indexOf(word) != -1) {
-    return true;
-  }
-  return false;
-}
 //検索条件に適合する（検索する必要のある）単語の場合にTrueを返す
 function needsSearch(morphologicalAnalysisResult, settingObj) {
   //NGWrod
   if (
-    isNgWord(morphologicalAnalysisResult["surface_form"], settingObj["ngWords"])
+    settingObj["ngWords"].indexOf(
+      morphologicalAnalysisResult["surface_form"]
+    ) != -1
   ) {
+    // NGwordsのリストに含まれているとき
     return false;
   }
   //品詞選択
   if (settingObj["selectNounType"] == "nounOnly") {
-    if (morphologicalAnalysisResult["pos"] != "名詞") {
-      return false;
-    }
+    // 名詞のみの場合
+    return !(morphologicalAnalysisResult["pos"] != "名詞");
   } else if (settingObj["selectNounType"] == "properNounOnly") {
-    if (morphologicalAnalysisResult["pos_detail_1"] != "固有名詞") {
-      return false;
-    }
+    // 固有名詞のみの場合
+    return !(morphologicalAnalysisResult["pos_detail_1"] != "固有名詞");
   } else if (settingObj["selectNounType"] == "whiteList") {
+    //ホワイトリストの場合
     if (
       settingObj["whiteList"].indexOf(
         morphologicalAnalysisResult["pos_detail_1"]
       ) == -1
     ) {
+      // pos_detail_1の値がホワイトリストになかった場合
       return false;
     } else if (
       settingObj["whiteList"].indexOf(
         morphologicalAnalysisResult["pos_detail_2"]
       ) == -1
     ) {
+      // pos_detail_2の値がホワイトリストになかった場合
       return false;
     } else if (
       settingObj["whiteList"].indexOf(
         morphologicalAnalysisResult["pos_detail_3"]
       ) == -1
     ) {
+      // pos_detail_3の値がホワイトリストになかった場合
       return false;
     }
   } else if (settingObj["selectNounType"] == "blackList") {
+    // ブラックリストの場合
     if (
       settingObj["blackList"].indexOf(
         morphologicalAnalysisResult["pos_detail_1"]
       ) != -1
     ) {
+      // pos_detail_1の値がブラックリストになかった場合
       return false;
     } else if (
       settingObj["blackList"].indexOf(
         morphologicalAnalysisResult["pos_detail_2"]
       ) != -1
     ) {
+      // pos_detail_2の値がブラックリストになかった場合
       return false;
     } else if (
       settingObj["blackList"].indexOf(
         morphologicalAnalysisResult["pos_detail_3"]
       ) != -1
     ) {
+      // pos_detail_3の値がブラックリストになかった場合
       return false;
     }
   } else {
@@ -76,12 +79,12 @@ function needsSearch(morphologicalAnalysisResult, settingObj) {
 //EntityLinkingを行う関数
 export default async function entityLinking(inputText, settingObj) {
   //設定
-  console.log('設定情報');
+  console.log("設定情報");
   console.log(settingObj);
 
   //形態素解析
   const morphologicalAnalysisResults = await morphologicalAnalysis(inputText);
-  console.log('形態素解析');
+  console.log("形態素解析");
   console.log(morphologicalAnalysisResults);
 
   //エンティティ候補取得
@@ -107,14 +110,17 @@ export default async function entityLinking(inputText, settingObj) {
   console.log(decideResults);
 
   //category付与
-  const addCategoryResult = (settingObj['checkedCategories'].length == 0) ? decideResults : await Promise.all(
-    decideResults.map(async (x) => {
-      if (x["linkedEntity"] != null) {
-        x["category"] = await fetchCategory(x["linkedEntity"]["id"]);
-      }
-      return x;
-    })
-  );
+  const addCategoryResult =
+    settingObj["checkedCategories"].length == 0
+      ? decideResults
+      : await Promise.all(
+          decideResults.map(async (x) => {
+            if (x["linkedEntity"] != null) {
+              x["category"] = await fetchCategory(x["linkedEntity"]["id"]);
+            }
+            return x;
+          })
+        );
 
   //リンキングテキスト生成
   const entityLinkedText = createLinkedText(
@@ -123,7 +129,7 @@ export default async function entityLinking(inputText, settingObj) {
     settingObj["displayPage"],
     {
       siteNameDisplayingEntity: settingObj["displayPage"],
-      checkedCategories: settingObj['checkedCategories']
+      checkedCategories: settingObj["checkedCategories"],
     }
   );
   return entityLinkedText;
