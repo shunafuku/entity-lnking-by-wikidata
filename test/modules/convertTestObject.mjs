1daa9605a7753b3dd120010d1383794805dc8403
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-05-24 21:31:23 +0900

    Update convertTestObject.mjs

---
+++ test/modules/convertTestObject.mjs
@@ -0,0 +1,37 @@
+function convert2TestObject(inputText) {
+    let outputText = '';
+    let ELResultArray = [];
+    const regexATag = RegExp('(<a.*?>.*?</a>)');
+    const splits = inputText.split(regexATag);
+    splits.forEach((x) => {
+      if (regexATag.test(x)) {
+        //aタグの値を取得
+        const regexATagValue = RegExp('<a.*?>(.*?)</a>');
+        const aTagValue = regexATagValue.exec(x)[1];
+        //aタグのurlを取得
+        const regexATagHref = RegExp('<a.*?href=[\'"](.*?)[\'"].*?>.*?</a>');
+        const aTagHref = regexATagHref.exec(x)[1];
+        //aタグの値の開始位置と終了位置を取得
+        const startPosition = outputText.length;
+        const endPosition = startPosition + aTagValue.length - 1;
+        //出力テキストに、aタグの値を追加
+        outputText += aTagValue;
+        //ELの結果を設定
+        ELResult = {
+          startPosition: startPosition,
+          endPosition: endPosition,
+          link: aTagHref,
+          label: aTagValue,
+        };
+        //ELの結果の配列に追加
+        ELResultArray.push(ELResult);
+      } else {
+        //出力テキストに、aタグの値を追加
+        outputText += x;
+      }
+    });
+    return {
+      outputText: outputText,
+      ELResultArray: ELResultArray,
+    };
+  }
