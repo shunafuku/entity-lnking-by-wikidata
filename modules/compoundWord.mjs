c5e4126786cbea26801b53f504094cf0b77008fb
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-05-24 11:16:14 +0900

    Update compoundWord.mjs

--- modules/compoundWord.mjs
+++ modules/compoundWord.mjs
@@ -1,11 +1,11 @@
-import searchEntityCandidate from "./searchEntity.mjs";
+import searchEntityCandidate from './searchEntity.mjs';
 
-function collectCompoundWordCandidate(morphologicalAnalysisResult) {
+function weldCompoundWordCandidate(morphologicalAnalysisResult) {
   let kouhoArray = new Array();
   let isExNoun = false;
   let currentNum = -1;
   morphologicalAnalysisResult.forEach((x) => {
-    if (x["pos"] == "名詞") {
+    if (x['pos'] == '名詞') {
       if (isExNoun) {
         kouhoArray[currentNum].push(x);
       } else {
@@ -25,16 +25,14 @@ function collectCompoundWordCandidate(morphologicalAnalysisResult) {
 async function exists(word) {
   const hoge = await searchEntityCandidate(word, {
     apiLimit: 1,
-    searchType: "exactMatchSearch",
+    searchType: 'exactMatchSearch',
   });
   return hoge.length;
-  const answerTextArray = ["東京スカイツリー", "先進国首脳会議"];
-  return answerTextArray.includes(word);
 }
 
 async function decideCompoundWord(inputTextArray) {
   inputTextArray.forEach((x) => {
-    x["flag"] = false;
+    x['flag'] = false;
   });
   let array = [];
 
@@ -47,40 +45,40 @@ async function decideCompoundWord(inputTextArray) {
     for (let i = 0; i < patternCount; i++) {
       const flagsArray = inputTextArray
         .slice(i, i + compoundWordNum)
-        .map((x) => x["flag"]);
+        .map((x) => x['flag']);
       if (flagsArray.length == 1) {
         if (!flagsArray[0]) {
           const compoundWord = inputTextArray
             .slice(i, i + compoundWordNum)
-            .map((x) => x["surface_form"])
-            .join("");
+            .map((x) => x['surface_form'])
+            .join('');
           array.push({
-            pos: "名詞",
-            pos_detail_1: "*",
-            pos_detail_2: "*",
-            pos_detail_3: "*",
+            pos: '名詞',
+            pos_detail_1: '*',
+            pos_detail_2: '*',
+            pos_detail_3: '*',
             surface_form: compoundWord,
-            word_position: i + inputTextArray[0]["word_position"],
+            word_position: i + inputTextArray[0]['word_position'],
           });
         }
       } else if (flagsArray.every((flag) => !flag)) {
         //flagが全部falseなら実行
         const compoundWord = inputTextArray
           .slice(i, i + compoundWordNum)
-          .map((x) => x["surface_form"])
-          .join("");
+          .map((x) => x['surface_form'])
+          .join('');
         await exists(compoundWord).then((result) => {
           if (result) {
             array.push({
-              pos: "名詞",
-              pos_detail_1: "*",
-              pos_detail_2: "*",
-              pos_detail_3: "*",
+              pos: '名詞',
+              pos_detail_1: '*',
+              pos_detail_2: '*',
+              pos_detail_3: '*',
               surface_form: compoundWord,
-              word_position: i + inputTextArray[0]["word_position"],
+              word_position: i + inputTextArray[0]['word_position'],
             });
             for (let j = i; j < i + compoundWordNum; j++) {
-              inputTextArray[j]["flag"] = true;
+              inputTextArray[j]['flag'] = true;
             }
           }
         });
@@ -88,13 +86,13 @@ async function decideCompoundWord(inputTextArray) {
     }
   }
   const hogehoge = array.sort((a, b) => {
-    return a["word_position"] - b["word_position"];
+    return a['word_position'] - b['word_position'];
   });
   return hogehoge;
 }
 
 export default async function searchCompoundWord(inputTextArray) {
-  const compoundWordCandidate = collectCompoundWordCandidate(inputTextArray);
+  const compoundWordCandidate = weldCompoundWordCandidate(inputTextArray);
   let resultArray = [];
   for (const x of compoundWordCandidate) {
     if (x.length == 1) {
