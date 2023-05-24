3e296f686c5ac71800b3698586f40ee4a02ece58
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-05-24 17:49:02 +0900

    Update sparql.mjs

--- modules/sparql.mjs
+++ modules/sparql.mjs
@@ -1,16 +1,16 @@
 export default async function fetchSparql(endpointUrl, sparqlQuery) {
-  const fullUrl = endpointUrl + "?query=" + encodeURIComponent(sparqlQuery);
+  const fullUrl = endpointUrl + '?query=' + encodeURIComponent(sparqlQuery);
   const headers = {
-    Accept: "application/sparql-results+json",
+    Accept: 'application/sparql-results+json',
   };
-  return fetch(fullUrl, { headers, method: "GET", mode: "cors" })
+  return fetch(fullUrl, { headers, method: 'GET', mode: 'cors' })
     .then((body) => {
       if (!body.ok) {
-        console.error("サーバーエラー");
+        console.error('サーバーエラー');
       }
       return body.json();
     })
     .catch((error) => {
-      console.error("通信に失敗しました", error);
+      console.error('通信に失敗しました', error);
     });
 }
