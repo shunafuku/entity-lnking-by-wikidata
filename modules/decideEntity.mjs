214cb8207c85a323c0d8a27ff531c85d8435fab7
shintaro fukuda <74655911+shunafuku@users.noreply.github.com>
2023-05-24 17:51:06 +0900

    Update decideEntity.mjs

--- modules/decideEntity.mjs
+++ modules/decideEntity.mjs
@@ -1,13 +1,13 @@
 //エンティティ候補の並び替え
 function matchTypeFiltering(searchResults, selectMatchLabelType) {
-  if (selectMatchLabelType == "labelpriority") {
+  if (selectMatchLabelType == 'labelpriority') {
     return searchResults.map((result) => {
-      if (result["candidate"] != null) {
-        result["candidate"].sort((a, b) => {
-          if (a["matchType"] == "label" && b["matchType"] == "alias") {
+      if (result['candidate'] != null) {
+        result['candidate'].sort((a, b) => {
+          if (a['matchType'] == 'label' && b['matchType'] == 'alias') {
             return -1;
           }
-          if (a["matchType"] == "label" && b["matchType"] == "alias") {
+          if (a['matchType'] == 'label' && b['matchType'] == 'alias') {
             return 1;
           }
           return 0;
@@ -15,27 +15,26 @@ function matchTypeFiltering(searchResults, selectMatchLabelType) {
       }
       return result;
     });
-  } else if (selectMatchLabelType == "labelOnly") {
+  } else if (selectMatchLabelType == 'labelOnly') {
     return searchResults.map((result) => {
-      if (result["candidate"] != null) {
-        result["candidate"] = result["candidate"].filter(
-          (x) => x["matchType"] == "label"
+      if (result['candidate'] != null) {
+        result['candidate'] = result['candidate'].filter(
+          (x) => x['matchType'] == 'label'
         );
       }
       return result;
     });
-  } else if (selectMatchLabelType == "aliasOnly") {
+  } else if (selectMatchLabelType == 'aliasOnly') {
     return searchResults.map((result) => {
-      if (result["candidate"] != null) {
-        console.log(result["candidate"]);
-        result["candidate"] = result["candidate"].filter(
-          (x) => x["matchType"] == "alias"
+      if (result['candidate'] != null) {
+        console.log(result['candidate']);
+        result['candidate'] = result['candidate'].filter(
+          (x) => x['matchType'] == 'alias'
         );
       }
       return result;
     });
-  } else if(selectMatchLabelType == "default")
-  return searchResults;
+  } else if (selectMatchLabelType == 'default') return searchResults;
 }
 
 class EntityDecider {
@@ -55,16 +54,16 @@ export default async function decideEntity(searchResults, settingObj) {
   //並び替え
   const matchTypeFilteringResult = matchTypeFiltering(
     searchResults,
-    settingObj["selectMatchLabelType"]
+    settingObj['selectMatchLabelType']
   );
-  console.log("matchTypeFilteringResult");
+  console.log('matchTypeFilteringResult');
   console.log(matchTypeFilteringResult);
   return await Promise.all(
     matchTypeFilteringResult.map(async (result) => {
-      const decider = new EntityDecider(result["candidate"]);
-      result["linkedEntity"] = decider.arrayFront();
+      const decider = new EntityDecider(result['candidate']);
+      result['linkedEntity'] = decider.arrayFront();
       return {
-        word: result["surface_form"],
+        word: result['surface_form'],
         linkedEntity: decider.arrayFront(),
       };
     })
